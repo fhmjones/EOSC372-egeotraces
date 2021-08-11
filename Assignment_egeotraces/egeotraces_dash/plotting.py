f0c96013d07ca79be09f5f9ed597e6467e7bdf43
Jamie Friesen Byer <byerjamie@gmail.com>
2021-08-11 12:21:30 -0700

    reorder depth profiles

--- Assignment_egeotraces/egeotraces_dash/plotting.py
+++ Assignment_egeotraces/egeotraces_dash/plotting.py
@@ -24,7 +24,7 @@ def contains_colour(list_stations, colour):
             return True
     return False
 
-def get_colour():
+def get_colour(): #getting the next colour in the series to plot
     for c in colours:
         if contains_colour(click_stations, c) == False:
             return c
@@ -83,16 +83,16 @@ def update_x_range(fig, x_range, cruise):
         if cruise == 'GIPY0405':
             fig.update_xaxes(range=[-5, 25], row=1, col=1) #temp
             fig.update_xaxes(range=[-10, 650], row=1, col=6) #ratio
-            fig.update_xaxes(range=[24, 28], row=1, col=2)  # density
+            fig.update_xaxes(range=[24, 28], row=1, col=3)  # density
         elif cruise == 'GA03':
             fig.update_xaxes(range=[5, 35], row=1, col=1) #temp
             fig.update_xaxes(range=[-10, 50], row=1, col=6) #ratio
-            fig.update_xaxes(range=[22, 28], row=1, col=2)  # density
+            fig.update_xaxes(range=[22, 28], row=1, col=3)  # density
         elif cruise == 'GP02':
             fig.update_xaxes(range=[0, 30], row=1, col=1) #temp
             fig.update_xaxes(range=[-10, 250], row=1, col=6) #ratio
-            fig.update_xaxes(range=[21, 28], row=1, col=2)  # density
-        fig.update_xaxes(range=[30, 37], row=1, col=3) #salinity
+            fig.update_xaxes(range=[21, 28], row=1, col=3)  # density
+        fig.update_xaxes(range=[30, 37], row=1, col=2) #salinity
         fig.update_xaxes(range=[-2, 45], row=1, col=4) #nitrate
         fig.update_xaxes(range=[-0.1, 2], row=1, col=5) #iron
     elif x_range == 'fitted':
@@ -146,22 +146,21 @@ def clear_click_traces(fig):
 #initialize the profiles
 def initialize_profiles(cruise, x_range, y_range):
     #global click_stations
-    fig = make_subplots(rows=1, cols=6, subplot_titles=("<b>Temperature</b>", "<b>Sigma0*</b>", "<b>Salinity</b>", "<b>Nitrate</b>",
+    fig = make_subplots(rows=1, cols=6, subplot_titles=("<b>Temperature</b>", "<b>Salinity</b>", "<b>Sigma0*</b>", "<b>Nitrate</b>",
                                                         "<b>Iron</b>", "<b>Nitrate/Iron</b>"))
 
 
     # empty traces for hovered data
     figT = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
     figS = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
+    figD = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
     figN = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
     figI = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
     figR = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
-    figD = px.scatter(x=[None], y=[None], color_discrete_sequence=['blue'])
 
-    #print(figT.data[0])
     fig.add_trace(figT.data[0], row=1, col=1)
-    fig.add_trace(figD.data[0], row=1, col=2)
-    fig.add_trace(figS.data[0], row=1, col=3)
+    fig.add_trace(figS.data[0], row=1, col=2)
+    fig.add_trace(figD.data[0], row=1, col=3)
     fig.add_trace(figN.data[0], row=1, col=4)
     fig.add_trace(figI.data[0], row=1, col=5)
     fig.add_trace(figR.data[0], row=1, col=6)
@@ -170,16 +169,16 @@ def initialize_profiles(cruise, x_range, y_range):
     for i in range(8):
         #traces for clicked data
         figT = px.scatter(x=[None], y=[None])
-        figD = px.scatter(x=[None], y=[None])
         figS = px.scatter(x=[None], y=[None])
+        figD = px.scatter(x=[None], y=[None])
         figN = px.scatter(x=[None], y=[None])
         figI = px.scatter(x=[None], y=[None])
         figR = px.scatter(x=[None], y=[None])
 
 
         fig.add_trace(figT.data[0], row=1, col=1)
-        fig.add_trace(figD.data[0], row=1, col=2)
-        fig.add_trace(figS.data[0], row=1, col=3)
+        fig.add_trace(figS.data[0], row=1, col=2)
+        fig.add_trace(figD.data[0], row=1, col=3)
         fig.add_trace(figN.data[0], row=1, col=4)
         fig.add_trace(figI.data[0], row=1, col=5)
         fig.add_trace(figR.data[0], row=1, col=6)
@@ -194,8 +193,8 @@ def initialize_profiles(cruise, x_range, y_range):
 
     #customize x axes
     fig.update_xaxes(title_text='deg C', row=1, col=1)
-    fig.update_xaxes(title_text="kg/m\u00B3", row=1, col=2) #unicode for the m^3
-    fig.update_xaxes(title_text='Practical Salinity', row=1, col=3)
+    fig.update_xaxes(title_text='Practical Salinity', row=1, col=2)
+    fig.update_xaxes(title_text="kg/m\u00B3", row=1, col=3)  # unicode for the m^3
     fig.update_xaxes(title_text="umol/kg", row=1, col=4)
     fig.update_xaxes(title_text="nmol/kg", row=1, col=5)
     fig.update_xaxes(title_text="umol/nmol", row=1, col=6)
@@ -231,16 +230,16 @@ def update_profiles(hov_data, click_data, cruise, fig, x_range, y_range):
         #set_hov_lat_lon_values(hov_data)
 
         hov_xvals_temp, hov_yvals_temp = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Temperature')
-        hov_xvals_dens, hov_yvals_dens = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Density')
         hov_xvals_sal, hov_yvals_sal = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Salinity')
+        hov_xvals_dens, hov_yvals_dens = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Density')
         hov_xvals_nit, hov_yvals_nit = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Nitrate')
         hov_xvals_iron, hov_yvals_iron = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Iron')
         hov_xvals_ratio, hov_yvals_ratio = get_x_y_values(cruise, hov_station.lat, hov_station.lon, 'Ratio')
 
 
         fig.data[0].update(x=hov_xvals_temp, y=hov_yvals_temp)
-        fig.data[1].update(x=hov_xvals_dens, y=hov_yvals_dens)
-        fig.data[2].update(x=hov_xvals_sal, y=hov_yvals_sal)
+        fig.data[1].update(x=hov_xvals_sal, y=hov_yvals_sal)
+        fig.data[2].update(x=hov_xvals_dens, y=hov_yvals_dens)
         fig.data[3].update(x=hov_xvals_nit, y=hov_yvals_nit)
         fig.data[4].update(x=hov_xvals_iron, y=hov_yvals_iron)
         fig.data[5].update(x=hov_xvals_ratio, y=hov_yvals_ratio)
@@ -254,16 +253,16 @@ def update_profiles(hov_data, click_data, cruise, fig, x_range, y_range):
         for i in range(8):
             if i < len(click_stations):
                 click_xvals_temp, click_yvals_temp = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Temperature')
-                click_xvals_dens, click_yvals_dens = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Density')
                 click_xvals_sal, click_yvals_sal = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Salinity')
+                click_xvals_dens, click_yvals_dens = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Density')
                 click_xvals_nit, click_yvals_nit = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Nitrate')
                 click_xvals_iron, click_yvals_iron = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Iron')
                 click_xvals_ratio, click_yvals_ratio = get_x_y_values(cruise, click_stations[i].lat, click_stations[i].lon, 'Ratio')
 
 
                 fig.data[6 + i * 6].update(x=click_xvals_temp, y=click_yvals_temp, marker_color=click_stations[i].colour)
-                fig.data[7 + i * 6].update(x=click_xvals_dens, y=click_yvals_dens, marker_color=click_stations[i].colour)
-                fig.data[8 + i * 6].update(x=click_xvals_sal, y=click_yvals_sal, marker_color=click_stations[i].colour)
+                fig.data[7 + i * 6].update(x=click_xvals_sal, y=click_yvals_sal, marker_color=click_stations[i].colour)
+                fig.data[8 + i * 6].update(x=click_xvals_dens, y=click_yvals_dens, marker_color=click_stations[i].colour)
                 fig.data[9 + i * 6].update(x=click_xvals_nit, y=click_yvals_nit, marker_color=click_stations[i].colour)
                 fig.data[10 + i * 6].update(x=click_xvals_iron, y=click_yvals_iron, marker_color=click_stations[i].colour)
                 fig.data[11 + i * 6].update(x=click_xvals_ratio, y=click_yvals_ratio, marker_color=click_stations[i].colour)
@@ -351,7 +350,7 @@ def initialize_map(cruise):
 def switch_map(cruise, fig):
     fig.data = []
     fig = plot_stations(cruise)
-    
+
     fig = map_initialize_cruise(fig, cruise)  # initializes the click for the new cruise
 
     if cruise == 'GIPY0405':
